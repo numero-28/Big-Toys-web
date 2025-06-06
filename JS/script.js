823afd2e56e596666c9bc2de4eaf69614afa7da1
numero-28 <luischaza13@gmail.com>
2025-06-06 12:48:35 +0200

    .

--- JS/script.js
+++ JS/script.js
@@ -1,64 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-<<<<<<< Updated upstream
-    console.log("DOM listo");
-  
-    // const colIzq = document.querySelector('.col-izq');
-    // const colDer = document.querySelector('.col-der');
-    // const slideHeight = window.innerHeight * 1.05;
-  
-    // let lastScrollY = window.scrollY;
-  
-    // window.addEventListener('scroll', () => {
-    //   const scrollingDown = window.scrollY > lastScrollY;
-    //   lastScrollY = window.scrollY;
-  
-    //   [colIzq, colDer].forEach(col => {
-    //     const slides = Array.from(col.children);
-    //     if (!slides.length) return;
-  
-    //     if (scrollingDown) {
-    //       const firstSlide = slides[0];
-    //       const rect = firstSlide.getBoundingClientRect();
-    //       console.log("Scrolling down. firstSlide bottom:", rect.bottom);
-  
-    //       if (rect.bottom < 0) {
-    //         col.appendChild(firstSlide);
-    //         window.scrollBy(0, -slideHeight);
-    //       }
-  
-    //     } else {
-    //       const lastSlide = slides[slides.length - 1];
-    //       const rect = lastSlide.getBoundingClientRect();
-    //       console.log("Scrolling up. lastSlide top:", rect.top);
-  
-    //       if (rect.top > window.innerHeight) {
-    //         col.insertBefore(lastSlide, slides[0]);
-    //         window.scrollBy(0, slideHeight);
-    //       }
-    //     }
-    //   });
-    // });
-
-    const $colIzq = $('.col-izq');
-    const $colDer = $('.col-der');
-
-    $colIzq.on('mouseenter', function () {
-        $colIzq.css('overflow-y', 'auto');
-        $colDer.css('overflow-y', 'hidden');
-    });
-
-    $colDer.on('mouseenter', function () {
-        $colDer.css('overflow-y', 'auto');
-        $colIzq.css('overflow-y', 'hidden');
-    });
-
-    $colIzq.on('mouseleave', function () {
-        $colIzq.css('overflow-y', 'hidden');
-    });
-
-    $colDer.on('mouseleave', function () {
-        $colDer.css('overflow-y', 'hidden');
-=======
   console.log("DOM listo");
 
   const colIzq = document.querySelector('.col-izq');
@@ -96,7 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
           window.scrollBy(0, slideHeight);
         }
       }
->>>>>>> Stashed changes
     });
   });
 });
